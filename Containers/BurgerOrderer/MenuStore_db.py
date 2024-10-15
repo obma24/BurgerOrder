aef201c1ce0ac2ad5c06e288d4dfb7d45a815974
obma24 <obma24@student.bth.se>
2024-10-15 16:26:21 +0200

    Changing swedish to english
    .

--- Containers/BurgerOrderer/MenuStore_db.py
+++ Containers/BurgerOrderer/MenuStore_db.py
@@ -1,17 +1,14 @@
 from pymongo import MongoClient
 
-# Anslut till MongoDB
 client = MongoClient('mongo', 27017)
-db = client.burger_orderer  # Välj databasen för burger orderer
+db = client.burger_orderer 
 
-# Lista över hamburgare att införa i databasen
 burgers = [
     {"name": "Cheeseburger"},
     {"name": "Bacon Burger"},
     {"name": "Vegan Burger"}
 ]
 
-# Lista över anpassningar att införa i databasen
 customizations = [
     {"name": "Extra Cheese"},
     {"name": "No Pickles"},
@@ -19,10 +16,9 @@ customizations = [
     {"name": "Double Patty"}
 ]
 
-# Rensa tidigare data och inför ny data i databasen
-db.burgers.drop()  # Tar bort tidigare hamburgare
-db.burgers.insert_many(burgers)  # Lägger till hamburgare
-db.customizations.drop()  # Tar bort tidigare anpassningar
-db.customizations.insert_many(customizations)  # Lägger till anpassningar
+db.burgers.drop()
+db.burgers.insert_many(burgers)
+db.customizations.drop()
+db.customizations.insert_many(customizations)
 
 print("Databasen är populär med hamburgare och anpassningar.") #test
